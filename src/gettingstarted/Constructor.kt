7a8c27da5b332d9de881d9663009d04d61c6e752
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-01-02 16:04:31 +0200

    intial commit

---
+++ src/gettingstarted/Constructor.kt
@@ -0,0 +1,17 @@
+package gettingstarted
+
+class Student(val name: String, age: Int) { // primary constructor is used to intialize the class 
+    var message: String = "HI "
+
+    // secondary constructor used to add extra logic, class can have more than one of them but all must call primary
+    //  this(lastName , age) --> used to call primary constructor
+    constructor(lastName: String, age: Int, message: String) : this(lastName, age) {
+        this.message = message
+        println("HI from Secondary ")
+    }
+}
+
+fun main(args: Array<String>) {
+    val s1 = Student("Mahmoud", 22, "No Play with mE Again ")
+    println("name is ${s1.name} message is  --> ${s1.message}")
+}
